05e59f474731c765211e30de8c8bb400f40c70f2
SecUpwN <secupwn@z1p.biz>
2014-06-01 13:52:33 +0000

    Create wrapper.sh

---
+++ wrapper.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Spotify-Adkiller Respository: https://github.com/SecUpwN/Spotify-AdKiller/
+# Feel free to contribute improvements and suggestions to this funky script!
+
+# Wrapper-Script to automatically start Spotify-AdKiller when Spotify starts
+
+# IMPORTANT REMINDER! CAREFULLY READ AND UNDERSTAND THIS PART. THANK YOU.
+# -----------------------------------------------------------------------
+# Spotify is a fantastic service and worth every penny.
+# This script is *NOT* meant to circumvent buying premium.
+# Please do consider switching to premium to support Spotify!
+# -----------------------------------------------------------------------
+
+spotify "$@" > /dev/null 2>&1 &
+sleep 20
+if [[ -z "$(pgrep spotify-aut)" ]]
+  then
+      spotify-automute > /dev/null 2>&1 &
+fi
