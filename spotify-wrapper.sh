7394c0a43f0aad5723d608dd3a7f7750131aaf3d
Feltzer <feltzer43324@gmail.com>
2014-06-03 14:52:51 +0200

    wait for spotify to launch, make sure to only launch adkiller once

--- spotify-wrapper.sh
+++ spotify-wrapper.sh
@@ -12,9 +12,39 @@
 # Please do consider switching to premium to support Spotify!
 # -----------------------------------------------------------------------
 
-$HOME/bin/*spotify "" > /dev/null 2>&1 &
-sleep 20
-if [[ -z "" ]]
+# WARNING: If you use this wrapper please make sure to ALWAYS use it
+#          If you start Spotify without it, there's a chance that it will
+#          stay muted and will have to be manually be unmuted through pactl
+#
+#          Unmute command (just in case):
+#          for i in $(pactl list | grep -E '(^Sink Input)|(media.name = \"Spotify\"$)' \
+#          | cut -d \# -f2 | grep -v Spotify); do pactl set-sink-input-mute "$i" no; done
+
+WMTITLE="Spotify - Linux Preview"
+COUNTER="0"
+
+spotify "$@" > /dev/null 2>&1 &
+
+# wait for spotify to launch
+# if spotify not launched after 50 seconds
+# exit script
+while true; do
+  if [[ "$COUNTER" = "10" ]]
+    then
+        echo "Error: Spotify not found."
+        exit 1
+  fi
+  echo "##Waiting for Spotify##"
+  xprop -name "$WMTITLE" WM_ICON_NAME > /dev/null 2>&1
+  if [[ "$?" == "0" ]]; then
+    break
+  fi
+  COUNTER=$(( COUNTER + 1 ))
+  sleep 5
+done
+
+# only launch script if it isn't active already
+if [[ -z "$(pgrep spotify-adk)" ]]
   then
-$HOME/bin/spotify-adkiller.sh > /dev/null 2>&1 &
+      spotify_adkiller.sh > /dev/null 2>&1 &
 fi
