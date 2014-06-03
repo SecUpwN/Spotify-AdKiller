2e90773b5cc12222de5aeeab5cde26532992dc31
Feltzer <feltzer43324@gmail.com>
2014-06-03 14:51:58 +0200

    typos, added comments, added volume option, script now restores settings on exit and ummutes on start, replaced pactl with pacmd (safer way to get sink), various other improvements

--- spotify-adkiller.sh
+++ spotify-adkiller.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Spotify-Adkiller Respository: https://github.com/SecUpwN/Spotify-AdKiller/
+# Spotify-Adkiller Repository: https://github.com/SecUpwN/Spotify-AdKiller/
 # Feel free to contribute improvements and suggestions to this funky script!
 
 # Spotify-AdKiller-Mode: Automute-Continuous
@@ -33,19 +33,40 @@
 # USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # -----------------------------------------------------------------------
 
-# Settings
+## Settings
 
+# volume of local playback
+VOLUME=80
+# local music directory to choose tracks from
 LOCALMUSIC="$HOME/Musi*"
+# alert to play when switching to local playback
 ALERT="/usr/share/sounds/gnome/default/alerts/glass.ogg"
-PLAYER="mpv --vo null"
+# local music player (alternatives: cvlc, mpg321)
+PLAYER="mpv --vo null --colume=$VOLUME"
 
-# VAR
+## VAR
 
 WMTITLE="Spotify - Linux Preview"
 ADMUTE=0
 PAUSED=0
+INITIALRUN=1
 
-# FCT
+BINARY="spotify"
+
+## FCT
+
+restore_settings(){
+    # I would love to restore the pactl settings here
+    # but unfortunately it's impossible; pactl can only control
+    # active sinks, i.e. if Spotify isn't running we can't control its
+    # mute state
+    echo "##Restoring settings##"
+    # terminate local music playback                                      
+    killall -9 "$PLAYER"  # problematic, will kill all $PLAYER instances
+                          # would love to use the PID but can't get it out of the
+                          # freaking subshell   
+}
+
 
 print_horiz_line(){
     printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
@@ -57,8 +78,11 @@ spotify_playpause(){
 }
 
 get_pactl_nr(){
-    LC_ALL=C pactl list | grep -E '(^Sink Input)|(media.name = \"Spotify\"$)' | cut -d \# -f2 \
-    | grep -v Spotify
+    LC_ALL=C pacmd list-sink-inputs | awk -v binary="$BINARY" '
+            $1 == "index:" {idx = $2} 
+            $1 == "application.process.binary" && $3 == "\"" binary "\"" {print idx; exit}
+        '
+    # awk script by Glenn Jackmann (http://askubuntu.com/a/180661/81372)
 }
 
 player(){
@@ -70,66 +94,82 @@ player(){
                       # subesequent actions like unmuting Spotify
 }
 
-# MAIN
+## PREP
+
+# make sure to restore settings upon exit
+trap restore_settings EXIT
+
+## MAIN
 
 xprop -spy -name "$WMTITLE" WM_ICON_NAME |
 while read -r XPROPOUTPUT; do
     XPROP_TRACKDATA="$(echo "$XPROPOUTPUT" | cut -d \" -f 2 )"
     DBUS_TRACKDATA="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify / \
     org.freedesktop.MediaPlayer2.GetMetadata | grep xesam:title -A 1 | grep variant | cut -d \" -f 2)"
-
+    
     echo "XPROP:    $XPROP_TRACKDATA"
     echo "DBUS:     $DBUS_TRACKDATA"
-
+    
     if [[ "$XPROP_TRACKDATA" = "Spotify" ]]
       then
-          echo "PAUSED:   Yes"
+          echo "PAUSED:      Yes"
           PAUSED="1"
       else
           PAUSED="0"
-          echo "PAUSED:   No"
+          echo "PAUSED:      No"
+          if [[ "$INITIALRUN" = 1 ]] # unmute on first track played
+            then
+                for PACTLNR in $(get_pactl_nr); do
+                  pactl set-sink-input-mute "$PACTLNR" no > /dev/null 2>&1 ## unmute
+                  echo "##INITIAL: Unmuting sink $PACTLNR##"
+                done
+                INITIALRUN="0"
+                print_horiz_line
+                continue
+          fi
     fi
-
-    if [[ "$PAUSED" = "1" || "$XPROP_TRACKDATA" =~ $DBUS_TRACKDATA ]]
+    
+    if [[ "$PAUSED" = "1" || "$XPROP_TRACKDATA" =~ "$DBUS_TRACKDATA" ]]
       then
-          echo "AD:       No"
+          echo "AD:          No"
           if [[ "$ADMUTE" = "1" ]]
             then
                 if ps -p $ALTPID > /dev/null 2>&1       # if alternative player still running
                   then
-                      if [[ "$PAUSED" != "1" ]]         # and if track not yet paused
+                      if [[ "$PAUSED" != "1" ]]         ## and if track not yet paused
                         then
-                            spotify_playpause           # then pause
-                            echo "## Pausing Spotify until local playback finished ##"
+                            spotify_playpause           ### then pause
+                            echo "##Pausing Spotify until local playback finished##"
                       fi
-                      continue                          # reset loop
-                  else                                  # if player not running
+                      print_horiz_line
+                      continue                          ## reset loop
+                  else
                       for PACTLNR in $(get_pactl_nr); do
                           pactl set-sink-input-mute "$PACTLNR" no > /dev/null 2>&1 ## unmute
-                          echo "## Unmuting sink $PACTLNR ##"
-                          echo "## Switching back to Spotify ##"
+                          echo "##Unmuting sink $PACTLNR##"
+                          echo "##Switching back to Spotify##"
                       done
                 fi
           fi
           ADMUTE=0
       else
-          echo "AD:       Yes"
+          echo "AD:          Yes"
           if [[ "$ADMUTE" != "1" ]]
             then
                 for PACTLNR in $(get_pactl_nr); do
                     pactl set-sink-input-mute "$PACTLNR" yes > /dev/null 2>&1
-                    echo "## Muting sink $PACTLNR ##"
+                    echo "##Muting sink $PACTLNR##"
                 done
                 if ! ps -p $ALTPID > /dev/null 2>&1
                   then
-                      echo "## Switching to local playback ##"
+                      echo "##Switching to local playback##"
                       player > /dev/null 2>&1 &
                       ALTPID="$!"
                 fi
           fi
           ADMUTE=1
     fi
-    printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+    print_horiz_line
 done
 
 echo "Spotify not active. Exiting."
