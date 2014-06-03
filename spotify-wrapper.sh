4d20b60e79732193bdb1b061f76e6e507e30d521
SecUpwN <secupwn@z1p.biz>
2014-06-03 23:21:07 +0000

    Added Changelog and Disclaimer

--- spotify-wrapper.sh
+++ spotify-wrapper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# Spotify-Adkiller Respository: https://github.com/SecUpwN/Spotify-AdKiller/
+# Official Spotify-AdKiller Repository: https://github.com/SecUpwN/Spotify-AdKiller/
+# CHANGELOG: https://github.com/SecUpwN/Spotify-AdKiller/blob/master/CHANGELOG.md
 # Feel free to contribute improvements and suggestions to this funky script!
 
 # Wrapper-Script to automatically start Spotify-AdKiller when Spotify starts
@@ -10,6 +11,20 @@
 # Spotify is a fantastic service and worth every penny.
 # This script is *NOT* meant to circumvent buying premium.
 # Please do consider switching to premium to support Spotify!
+# -----------------------------------------------------------------------
+# This product is not endorsed, certified or otherwise approved in any way
+# by Spotify. Spotify is the registered trade mark of the Spotify Group.
+# -----------------------------------------------------------------------
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # -----------------------------------------------------------------------
 
 # WARNING: If you use this wrapper please make sure to ALWAYS use it
