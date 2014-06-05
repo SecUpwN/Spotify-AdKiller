116936945ebeb95fb250349e70e8a3b1e3431d56
Feltzer <feltzer43324@gmail.com>
2014-06-05 23:34:47 +0200

    Fixed empty XPROP_TRACKDATA when playback paused + made DBUS parsing more safe (possible fix for #7)

--- spotify-adkiller.sh
+++ spotify-adkiller.sh
@@ -166,11 +166,12 @@ choose_player
 ## MAIN
 
 while read -r XPROPOUTPUT; do
-    XPROP_TRACKDATA="$(echo "$XPROPOUTPUT" \
-    | sed -n 's/WM_ICON_NAME(COMPOUND_TEXT) = \"//p' \
-    | head -c -2 )"
+    # get string between first and last double-quotes   ↓
+    # FIXME: find a more elegant way to do this
+    XPROP_TRACKDATA="$(echo "$XPROPOUTPUT" | cut -d\" -f 2- | rev | cut -d\" -f 2- | rev)"
     DBUS_TRACKDATA="$(dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify / \
-    org.freedesktop.MediaPlayer2.GetMetadata | grep xesam:title -A 1 | grep variant | cut -d \" -f 2)"
+    org.freedesktop.MediaPlayer2.GetMetadata | grep xesam:title -A 1 | grep variant | \
+    cut -d\" -f 2- | rev | cut -d\" -f 2- | rev)"
     
     echo "XPROP:    $XPROP_TRACKDATA"
     echo "DBUS:     $DBUS_TRACKDATA"
